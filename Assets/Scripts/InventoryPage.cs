bc8117cbdcbb093a1639677503412b03890aa439
Connor <connorjamesmaider@hotmail.com>
2024-12-02 21:26:13 +0000

    Inventory code checkpoint
    inventory can be called in game with a key press and using scripts and prefabs, inventory size and other things can be called

---
+++ Assets/Scripts/InventoryPage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPage : MonoBehaviour
+{
+    [SerializeField]
+    private InventoryItem itemPrefab;
+
+    [SerializeField]
+    private RectTransform contentPanel;
+
+    List<InventoryItem> listOfItems = new List<InventoryItem>();
+
+    public void InitialiseInventory(int inventorySize)
+    {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            InventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel);
+            listOfItems.Add(uiItem);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
