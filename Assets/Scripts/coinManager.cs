bbb56e9c9bcf8f52c90c2cbcd35a1988bb920026
Connor <connorjamesmaider@hotmail.com>
2024-12-03 11:25:58 +0000

    progress

--- Assets/Scripts/coinManager.cs
+++ Assets/Scripts/coinManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class coinManager : MonoBehaviour
 {
     public int coinCount;
+    public Text coinText;
+    public GameObject Gate;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,11 @@ public class coinManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        coinText.text = "Coin Count:" + coinCount.ToString();
+
+        if(coinCount == 3)
+        {
+            Destroy(Gate);
+        }
     }
 }
